62dd879035ef694c6d43e21722c85328c31c0d60
asyncio886 <2409298768@qq.com>
2022-11-14 15:52:58 +0800

    初始化脚本更新一下bug

--- init.sh
+++ init.sh
@@ -4,9 +4,17 @@ do
 		echo $dir;
 		for problem_dir in `ls ${dir}` 
 		do
-			echo "初始化 ${dir}${problem_dir}";
-			init_file_name="init__.sh"
-			touch "${dir}${problem_dir}${init_file_name}"
+			base_problem_dir="${dir}${problem_dir}";
+			if [ -d  ${base_problem_dir} ];then
+				echo "初始化 ${base_problem_dir}";
+				init_file_name="init__.sh"
+				target_file="${base_problem_dir}${init_file_name}";
+				if [ -ne $target_file ];then
+					touch $target_file;
+				else
+					echo "${target_file}已经初始化";
+				fi
+			fi
 		done
 	fi
 done
