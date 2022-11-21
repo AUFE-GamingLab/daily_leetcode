8d1d47bb1a875a0852de503f981a2df417208212
asyncio886 <2409298768@qq.com>
2022-11-21 14:28:02 +0800

    检查脚本

--- check.sh
+++ check.sh
@@ -6,12 +6,12 @@ do
 		do
 			base_problem_dir="${dir}${problem_dir}";
 			if [ -d  ${base_problem_dir} ];then
-				echo "${problem_dir}"
+				echo "${problem_dir:0:-1}"
 				current_dir=`ls ${base_problem_dir}`
 				for p_dir in $current_dir
 				do
 					if [ -d "${base_problem_dir}${p_dir}" ];then
-					echo ${p_dir}
+					echo ${p_dir:0:-1}
 					fi
 				done
 			fi
