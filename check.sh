aa06a74c791d6b4c126dc025201e46c2e0361055
asyncio886 <2409298768@qq.com>
2022-11-21 14:19:07 +0800

    添加一个检查脚本

---
+++ check.sh
@@ -0,0 +1,20 @@
+for dir in `ls`
+do
+	if [ -d $dir ];then
+		echo $dir;
+		for problem_dir in `ls ${dir}` 
+		do
+			base_problem_dir="${dir}${problem_dir}";
+			if [ -d  ${base_problem_dir} ];then
+				echo "${problem_dir}"
+				current_dir=`ls ${base_problem_dir}`
+				for p_dir in $current_dir
+				do
+					if [ -d "${base_problem_dir}${p_dir}" ];then
+					echo ${p_dir}
+					fi
+				done
+			fi
+		done
+	fi
+done
